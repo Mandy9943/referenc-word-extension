e608e0f575f213fe823b1a60d3a2638d7131f645
Armando Cesar <armandoc9943@gmail.com>
2025-11-18 11:51:13 -0300

    fix(taskpane): Update version number from v2.0 to v2.1

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -277,7 +277,7 @@ const App: React.FC = () => {
             This add-in is optimized for Word and PowerPoint. Some features may not be available in other applications.
           </Text>
         )}
-        v2.0
+        v2.1
         {getStatusDisplay()}
       </div>
     </div>
